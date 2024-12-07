e57639ab656b9b0d59e18e2697e3e7f77136afc2
Joshua <joshua@creao.ai>
2024-12-06 19:09:10 -0800

    update url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/creao_black_logo.svg",
 
   // Set the production url of your site here
-  url: "https://creao.github.io",
+  url: "https://creaoai.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
