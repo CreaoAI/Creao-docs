3efc8cb0346992907f0f5c5c5641ce4fd2170f85
Joshua <joshua@creao.ai>
2024-12-09 21:48:11 -0800

    update social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/creao_black_logo.svg",
     navbar: {
       // title: "Creao",
       logo: {
